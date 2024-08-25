392a5ebd8fefd684fffd3b4545adfc8a2153b2b8
Kingshuk Sarkar <93638401+Kingshuk3006@users.noreply.github.com>
2024-08-26 00:17:09 +0530

    responsive added

--- src/app/publication/page.tsx
+++ src/app/publication/page.tsx
@@ -22,13 +22,11 @@ const PublicationPage = () => {
                 <div className="mb-4 md:mb-6 lg:mb-8 grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 justify-center">
                     <div>
                         <p className="mb-6 text-justify">
-                            Advocate Parag Mulhopadhyy  publication section,
+                            Advocate Parag Mukhopadhyay  publication section,
                             showcasing his expertise in legal matters and thought leadership.
                             His writings include insightful articles on contemporary legal
                             issues and landmark case analyses.
                         </p>
-
-
                         <a href="/assets/publication.pdf" download target="_blank"> <button className="btn-primary hover:text-primary">Download<img src="/icons/download.png" className="w-6" /></button></a>
                     </div>
                     <img src="/certificate.jpg" className="xl:w-[35rem] lg:w-[30rem] md:w-[25rem] w-[20rem]" />
--- src/components/Home/Publication.tsx
+++ src/components/Home/Publication.tsx
@@ -4,7 +4,7 @@ const Publication = () => {
   return (
     <div className="bottom-margin-content">
       <h2 className="heading mb-6 md:mb-8 lg:mb-12 text-center text-primary">Publication</h2>
-      <div className="mb-4 md:mb-6 lg:mb-8 flex justify-between gap-6 md:gap-8 lg:gap-20 xl:gap-36">
+      <div className="mb-4 md:mb-6 lg:mb-8 flex flex-wrap justify-between gap-6 md:gap-8 lg:gap-20 xl:gap-36">
         <div>
           <p className="mb-6 text-justify text-lg">
             Advocate Parag Mulhopadhyay publication, showcasing his expertise in
