474997836816994a7690a08b5df7dbc7256c47c7
Kingshuk3006 <kingsarkar3006@gmail.com>
2024-09-04 19:49:10 +0530

    blogs added

--- data/all-blogs.ts
+++ data/all-blogs.ts
@@ -1,11 +1,11 @@
 export const allBlogs = [
-    {
-        id: "564b18cb-c41b-4e70-b2b8-4335250a2382",
-        title: "Criminal justice system in india",
-        date: "25th Aug 2024",
-        brief:
-            "India's criminal justice system enforces law, punishes offenders, and prevents crime, following the adversarial model from British common law.",
-        blog: `<h1 id="criminal-justice-system-in-india">Criminal justice system in india</h1>
+  {
+    id: "564b18cb-c41b-4e70-b2b8-4335250a2382",
+    title: "Criminal justice system in india",
+    date: "25th Aug 2024",
+    brief:
+      "India's criminal justice system enforces law, punishes offenders, and prevents crime, following the adversarial model from British common law.",
+    blog: `<h1 id="criminal-justice-system-in-india">Criminal justice system in india</h1>
 <p> This system deals with the agencies of government that are enforcing by the government of law in the country Maintaining peace and harmony and treating criminal conduct known as criminal justice system.The aim of the criminal justice system is to ensure that every person who suffers injury or loss ay the hand of others is allowed to presentation his case and seeking justice. 
  According to <em>Hobbes</em> ,man is selfish by nature and can go to any extent for pleasure. Well said by Bentham,a person avoids pain and demands to seeking pleasure. 
 Thus in order,to regulate the conduct of a man a system was needed that could monitor his actions.The development of the criminal justice system is the same as the development of man</p>
@@ -19,7 +19,41 @@ Thus in order,to regulate the conduct of a man a system was needed that could mo
 The principal of Hinduism and other religions in india value human life and adhere to the princeipal of providing an equal opportunity to every single person to present his side of the story.Thus,the Indian criminal system depends on the maxim” let hundred people be aquitted and freed but one innocent person should never be convicted.”</p>
 
       `,
-        image:
-            "https://askusedu.com/blogdashboard/wp-content/uploads/2023/07/mba-after-law-llb.jpg",
-    },
+    image:
+      "https://askusedu.com/blogdashboard/wp-content/uploads/2023/07/mba-after-law-llb.jpg",
+  },
+  {
+    id: "a3ee21a2-eaa2-4089-b395-39fb064ad250",
+    title: "Civil Law",
+    date: "4th Sep 2024",
+    brief:
+      "Civil law governs disputes between individuals or organizations, addressing private rights and obligations, including contracts, property, and family matters.",
+    blog: `<h1 id="civil-law">Civil Law</h1>
+<p>Civil law is a legal system originating in Italy and France that has been adopted in large parts of the world. Modern civil law stems mainly from the Napoleonic code of the early 19th century. And it is a continuation of ancient  roman law.
+India’s legal system is drawn from three primary sources: the common law , religious law , and civil (‘ Romanist’ ) law.
+Civil law is a set of rules and regulations in India that help resolve non- criminal disputes between citizens. It includes law that govern personal affairs like
+Property, marriage, and ownership.
+Civil law in India includes:</p>
+<ul>
+<li><p><strong>Contract Law</strong>: Regulates, enforces, and interprets contracts.</p>
+</li>
+<li><p><strong>Property Law :</strong> Regulates property rights , including limitations on others’ property and rights to use or transfer one’s own property.</p>
+</li>
+<li><strong>Family Law :</strong> Regulates relationship and exchanges between family members .</li>
+<li><strong>Corporate Law :</strong> Govern the rights and obligations of corporate entities.</li>
+</ul>
+<p><strong>Burden of proof :</strong> In the case if civil law , the burden of creating proof before the law lies on the head of the complainant and    then the defendant who can refute what has been provided as proved by    complainant .
+<strong>Types of punishment :</strong> Civil law covers aspects of personal affairs where the guilty one needs to compensate for the injuries or the damages done by the guilty one to the plaintiff if proven guilty.
+<strong>Case of documenting party :</strong>  In the case of civil law , it is the private party that files the case in the court of law. On the other hand , in the case of criminal law, it is the state or the government that files a case against the defendant , who has to defend himself .
+<strong>Purpose of law :</strong> The purpose of civil law enforcement is to protect the rights of the citizen of the country, make sure that nothing wrong happens to them , and provide compensation for the wrong that has been done.</p>
+<h2 id="civil-law-in-india-">Civil law in India:</h2>
+<p>The code of civil procedure, 1908(CPC) is the primary law governing civil proceedings in india. Civil law is based on a logical and dynamic hierarchy that derives from Roman law.</p>
+<p>The law in india  is mainly governed by the code of civil procedure 1908 (CPC) which is a procedural law relating to the conduct of civil proceedings in India.</p>
+<p>A comprehensive said of guidelines and principles that are usually organized into codes and are understandable to the public and legal professional. A systematic framework based on a dynamic and logical hierarchy that as is roots in roman law and is reflected in the form of courts, which encourages cooperation predictability and discipline.</p>
+<p>A flexible system where general provisions allowing for change are included in civil code and avoided unnecessary detail.</p>
+<p>A System that is mostly legislative in nature, but that allows courts to use creative jurisprudence and interpretation to adapt the law to new needs and social changes.</p>
+`,
+    image:
+      "https://www.brentgeorgelaw.com/wp-content/uploads/2015/01/January-2015-Common-Civil-Law-Cases-Featured.jpg",
+  },
 ];
--- src/components/Home/HeroCarousel.tsx
+++ src/components/Home/HeroCarousel.tsx
@@ -4,55 +4,52 @@ import { SwiperSlide, Swiper } from "swiper/react";
 import "swiper/css/navigation";
 import { Autoplay, Navigation } from "swiper/modules";
 import "swiper/css";
-import { motion } from "framer-motion";
+import Image from "next/image";
 
 const HeroCarousel = () => {
-    return (
-        <div className="bottom-margin-content">
-            <Swiper
-                navigation={false}
-                slidesPerView={1}
-                modules={[Navigation, Autoplay]}
-                autoplay
-                loop={true}
-            >
-                <SwiperSlide>
-                    <img src="/hero-background/image1.png" className="relative h-full w-screen object-cover" />
-                    <div className="absolute top-1/2 -translate-y-1/2 md:left-[10%] left-[4%] flex gap-2 flex-col text-white">
-                        <h3 className="md:text-xl text-lg">Welcome to</h3>
-                        <h1 className="heading font-extrabold leading-relaxed">
-                            Adv.Parag Mukhopadhyay,
-                        </h1>
-                        <h3 className="text-2xl">Your <b className="text-primary font-extrabold">Criminal Defense</b> Expert.</h3>
-                        <button className="btn-primary w-fit mt-6">Learn More <img src="/icons/arrow.png" className="h-4" /></button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/hero-background/image2.png" className="relative h-full w-screen object-cover" />
-                    <div className="absolute top-1/2 -translate-y-1/2 md:left-[10%] left-[4%] flex gap-2 flex-col text-white">
-                        <h3 className="md:text-xl text-lg">Welcome to</h3>
-                        <h1 className="heading font-extrabold leading-relaxed">
-                            Adv.Parag Mukhopadhyay,
-                        </h1>
-                        <h3 className="text-2xl">Your <b className="text-primary font-extrabold">Criminal Defense</b> Expert.</h3>
-                        <button className="btn-primary w-fit mt-6">Learn More <img src="/icons/arrow.png" className="h-4" /></button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/hero-background/image3.png" className="relative h-full w-screen object-cover" />
-                    <div className="absolute top-1/2 -translate-y-1/2 md:left-[10%] left-[4%] flex gap-2 flex-col text-white">
-                        <h3 className="md:text-xl text-lg">Welcome to</h3>
-                        <h1 className="heading font-extrabold leading-relaxed">
-                            Adv.Parag Mukhopadhyay,
-                        </h1>
-                        <h3 className="text-2xl">Your <b className="text-primary font-extrabold">Criminal Defense</b> Expert.</h3>
-                        <button className="btn-primary w-fit mt-6" onClick={()=> window.location.href = "/#about"}>Learn More <img src="/icons/arrow.png" className="h-4" /></button>
-                    </div>
-                </SwiperSlide>
-
-            </Swiper>
-        </div >
-    );
+  return (
+    <div className="bottom-margin-content">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        navigation={false}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        style={{ height: "100vh" }}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Image
+            objectFit="cover"
+            priority={true}
+            src="/hero-background/image1.png"
+            alt="Slide 1"
+            layout="fill"
+          />
+          <div className="absolute top-1/2 -translate-y-1/2 md:left-[10%] left-[4%] flex gap-2 flex-col text-white">
+            <h3 className="md:text-xl text-lg">Welcome to</h3>
+            <h1 className="heading font-extrabold leading-relaxed">
+              Adv.Parag Mukhopadhyay,
+            </h1>
+            <h3 className="text-2xl">
+              Your{" "}
+              <b className="text-primary font-extrabold">Criminal Defense</b>{" "}
+              Expert.
+            </h3>
+            <button className="btn-primary w-fit mt-6">
+              Learn More <img src="/icons/arrow.png" className="h-4" />
+            </button>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default HeroCarousel;
