6ce60444ef6e31f193a1737b2b7448a95d604ee2
Kingshuk Sarkar <93638401+Kingshuk3006@users.noreply.github.com>
2024-09-07 19:37:24 +0530

    Update Publication.tsx

--- src/components/Home/Publication.tsx
+++ src/components/Home/Publication.tsx
@@ -5,7 +5,7 @@ const Publication = () => {
     <div className="bottom-margin-content flex flex-col justify-center items-center gap-6">
       <h2 className="heading text-center text-primary">Publication</h2>
       <p className="text-center text-lg">
-        Advocate Parag Mulhopadhyay publication, showcasing his expertise in
+        Advocate Parag Mukhopadhyay publication, showcasing his expertise in
         legal matters and thought leadership. His writings include
         insightful articles on contemporary legal issues and landmark case
         analyses.
